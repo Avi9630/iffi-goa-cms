c66dd02f827b5d5765d3a7aa0ee9db2edf06cf88
Avinash <avinashmca94@gmail.com>
2025-08-08 23:55:43 +0530

    Changes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>AdminLTE | Dashboard v3</title><!--begin::Accessibility Meta Tags-->
+    <title>IFFI-CMS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     <meta name="color-scheme" content="light dark">
     <meta name="theme-color" content="#007bff" media="(prefers-color-scheme: light)">
