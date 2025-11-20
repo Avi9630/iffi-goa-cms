04d19d16a6b6e90061994866c8551d9835462f40
Avi9630 <avinashmca94@gmail.com>
2025-11-20 14:04:28 +0530

    Changes

--- resources/views/press_release/index.blade.php
+++ resources/views/press_release/index.blade.php
@@ -21,6 +21,78 @@
             </div>
         </div>
     </div>
+
+    {{-- Search --}}
+    <div class="app-content mt-2">
+        <div class="container-fluid">
+            <div class="row g-4">
+                <div class="col-md-12">
+                    <div class="card card-primary card-outline mb-4">
+                        <div class="card-header">
+                            <div class="card-title">Search</div>
+                        </div>
+                        <form action="{{ route('pressRelease.search') }}" method="GET">
+                            @csrf @method('GET')
+                            <div class="card-body">
+                                <div class="row">
+
+                                    <div class="col-md-6 mb-3">
+                                        <label for="pr_category_id" class="form-label"><strong>Category</strong></label>
+                                        <select name="pr_category_id" id="pr_category_id"
+                                            class="form-select @error('pr_category_id') is-invalid @enderror">
+                                            <option value="" selected>Select Category</option>
+                                            <option value="1" {{ old('pr_category_id') == 1 ? 'selected' : '' }}>
+                                                PRESS RELEASES BY PIB
+                                            </option>
+                                            <option value="2" {{ old('pr_category_id') == 2 ? 'selected' : '' }}>
+                                                MEDIA COVERAGE (NON PIB)
+                                            </option>
+                                        </select>
+                                        @error('pr_category_id')
+                                            <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="col-md-6 mb-3">
+                                        <label for="title" class="form-label"><strong>Title</strong></label>
+                                        <input type="text" name="title" id="title" class="form-control"
+                                            value="{{ isset($payload['title']) ? $payload['title'] : '' }}">
+                                        @error('title')
+                                            <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="col-md-6 mb-3">
+                                        <label for="year" class="form-label"><strong>Year</strong></label>
+                                        <select name="year" id="year"
+                                            class="form-select @error('year') is-invalid @enderror">
+                                            <option value="" selected>Select Year</option>
+                                            <option value="2025"
+                                                {{ isset($payload['year']) && $payload['year'] == 2025 ? 'selected' : '' }}>
+                                                2025
+                                            </option>
+                                            <option value="2024"
+                                                {{ isset($payload['year']) && $payload['year'] == 2024 ? 'selected' : '' }}>
+                                                2024
+                                            </option>
+                                        </select>
+                                        @error('year')
+                                            <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     <div class="app-content">
         <div class="container-fluid">
             <div class="row">
